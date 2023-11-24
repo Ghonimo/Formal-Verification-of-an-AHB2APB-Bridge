4773820c828a1a03d705b42a80e723b0dfba0a4e
Ghonimo <ghonim@localhost.ghonim>
2023-11-24 02:46:17 -0500

    rearranged the directories and added aep and fxp run scripts for different design levels

---
+++ run/fxp_run_ahb_slave.tcl
@@ -0,0 +1,17 @@
+
+# Select AEP as the VC Formal App mode
+set_fml_appmode FXP
+set design AHB_slave_interface
+
+set_fml_var fml_aep_unique_name true
+read_file -top $design -format sverilog -sva \
+-aep all -vcs {../RTL/AHB_Slave_Interface.v +define+INLINE_SVA}
+#read_waiver_file -elfiles aep.el
+
+# Creating clock and reset signals
+create_clock Hclk -period 100 
+create_reset Hresetn -sense low
+
+# Runing a reset simulation
+sim_run -stable 
+sim_save_reset
